9a58bdfe1926f9ab55e43cdb794f255a7d8f885e
Richard Newton <therichardnewton+github@gmail.com>
2022-07-22 12:28:48 +0100

    Initial commit
    A Go program to make URL requests that will generate lots of ad traffic. 1000 requests in roughly 2 minutes.

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # http_ad_testing
  small Go program to make HTTP requests for Ad testing
+
+config.json looks something like:
+
+{
+"prefix": "URL PREFIX",
+"suffix": "URL SUFFIX"
+}
+
+mac_list is just a list of MAC addresses or device IDs or whatever you need to mix into the URL
+
+main.go will mix the devices into the URL and make a request to that full URL
---
+++ main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+    "encoding/json"
+    "net/http"
+    "bufio"
+    "fmt"
+    "log"
+    "os"
+)
+
+type configuration struct {
+    Prefix      string
+    Suffix      string
+}
+
+func main() {
+    var config string = "/Users/richard/Desktop/go/build/conf.json"
+    var macs string = "/Users/richard/Desktop/go/build/mac_list"
+    prefix, suffix := fixes(config)
+
+    maclist, err := os.Open(macs)
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer maclist.Close()
+
+    scanner := bufio.NewScanner(maclist)
+    // optionally, resize scanner's capacity for lines over 64K, see next example
+    for scanner.Scan() {
+        resp, err := http.Get(prefix+scanner.Text()+suffix)
+        if err != nil {
+            log.Fatalln(err)
+        }
+
+        fmt.Println(scanner.Text(), resp.StatusCode)
+    }
+
+    if err := scanner.Err(); err != nil {
+        log.Fatal(err)
+    }
+
+}
+
+func fixes(config string) (prefix, suffix string) {
+    // open a file
+    conf_file, _ := os.Open(config)
+
+    //  Close file
+    defer conf_file.Close()
+
+    //NewDecoder creates a*decoder that reads and decodes the JSON object from File. The decoder has its own buffer and may read some JSON data in advance.
+    decoder := json.NewDecoder(conf_file)
+
+    conf := configuration{}
+    //DECODE Read a JSON coding value from the input stream and save it in the V -Direct value
+    err := decoder.Decode(&conf)
+    if err != nil {
+        fmt.Println("Error:", err)
+    }
+    prefix = conf.Prefix
+    suffix = conf.Suffix
+    return
+}
